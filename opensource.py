2928a25c29e5a8737936fd0d4f9fbfbb3e1c8a18
Heejae <jhj967878@naver.com>
2016-10-24 18:34:49 +0900

    add \!

--- opensource.py
+++ opensource.py
@@ -28,28 +28,28 @@ def main():
 		# 사용자가 가위를 냈을 경우
 		if(com_finger == 1):
 			if(my_finger == 1):
-				print("컴퓨터가 낸 것은 가위입니다. -> Draw")
+				print("컴퓨터가 낸 것은 가위입니다. -> Draw!!")
 			elif(my_finger == 2):
-				print("컴퓨터가 낸 것은 가위입니다. -> Win!")
+				print("컴퓨터가 낸 것은 가위입니다. -> Win!!")
 			elif(my_finger == 3):
-				print("컴퓨터가 낸 것은 가위입니다. -> Lose")
+				print("컴퓨터가 낸 것은 가위입니다. -> Lose!!")
 		
 		# 사용자가 바위를 냈을 경우
 		elif(com_finger == 2):
 			if(my_finger == 1):
-				print("컴퓨터가 낸 것은 바위입니다. -> Lose")
+				print("컴퓨터가 낸 것은 바위입니다. -> Lose!!")
 			elif(my_finger == 2):
-				print("컴퓨터가 낸 것은 가위입니다. -> Draw")
+				print("컴퓨터가 낸 것은 가위입니다. -> Draw!!")
 			elif(my_finger == 3):
-				print("컴퓨터가 낸 것은 가위입니다. -> Win!")
+				print("컴퓨터가 낸 것은 가위입니다. -> Win!!")
 		
 		# 사용자가 보를 냈을 경우
 		elif(com_finger == 3):
 			if(my_finger == 1):
-				print("컴퓨터가 낸 것은 바위입니다. -> Win!")
+				print("컴퓨터가 낸 것은 바위입니다. -> Win!!")
 			elif(my_finger == 2):
-				print("컴퓨터가 낸 것은 가위입니다. -> Lose")
+				print("컴퓨터가 낸 것은 가위입니다. -> Lose!!")
 			elif(my_finger == 3):
-				print("컴퓨터가 낸 것은 가위입니다. -> Draw")
+				print("컴퓨터가 낸 것은 가위입니다. -> Draw!!")
 
 main()
